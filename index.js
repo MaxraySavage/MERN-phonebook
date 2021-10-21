3c71c473c1d86a5bfcbed4d1027acb49b9861a8a
MaxraySavage <maxraysavage@gmail.com>
2021-10-20 20:33:08 -0400

    static serve react build

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-object'))
 app.use(express.json())
 
+app.use(express.static('build'))
+
 let persons = [
     { 
       "id": 1,
